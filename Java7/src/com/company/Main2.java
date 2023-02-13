097a7414e4dfdaaf68c8387d10eba2804179d113
ArturPromon <artur.shovkunenko@gmail.com>
2023-02-13 20:04:09 +0200

    First lesson, types of variables

---
+++ Java7/src/com/company/Main2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main2 {
+    public static void main(String[] args){
+        int num = 10;
+        float num2 = 10.5f;
+        double num3 = 20.8d;
+        // byte [-128, 128)
+        byte num4 = -128;
+        short num5 = 32000;
+        long num6 = 124124124;
+        String str = "Hello";
+        char sym = '*';
+        // true false
+        boolean isTrue = true;
+    }
+}

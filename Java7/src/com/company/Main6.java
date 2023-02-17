0be6ffd0af45fc477318a3b8c3db80d7695dc306
ArturPromon <artur.shovkunenko@gmail.com>
2023-02-17 20:12:49 +0200

    switch case

---
+++ Java7/src/com/company/Main6.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Please enter some value: ");
+        int num = scn.nextInt();
+        switch(num){
+            case 5:
+                System.out.println("num = 5");
+                break;
+            case 10:
+                System.out.println("num = 10");
+                break;
+            case 15:
+                System.out.println("num = 15");
+                break;
+            default:
+                System.out.println("num is some number!");
+        }
+    }
+}

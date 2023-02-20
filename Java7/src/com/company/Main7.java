88a26a2d88cbe71ebad72d99e0ab5b9d3faaa3f7
ArturPromon <artur.shovkunenko@gmail.com>
2023-02-20 18:47:10 +0200

    while

--- Java7/src/com/company/Main7.java
+++ Java7/src/com/company/Main7.java
@@ -9,29 +9,37 @@ public class Main7 {
         char sym;
         System.out.println("Please enter first number: ");
         num = scn.nextInt();
-        System.out.println("Please enter second number: ");
-        num2 = scn.nextInt();
         System.out.println("Please enter an operation(+, -, *, /): ");
         sym = scn.next().charAt(0);
-        switch(sym){
-            case '+' :
-                result = num + num2;
-                System.out.println(result);
+        while(true){
+            if(sym != '+' && sym != '-' && sym != '*' && sym != '/'){
+                System.out.println("Incorrect an operation! Please try again(+, -, *, /): ");
+                sym = scn.next().charAt(0);
+            }else{
+                System.out.println("Please enter second number: ");
+                num2 = scn.nextInt();
+                switch(sym){
+                    case '+' :
+                        result = num + num2;
+                        System.out.println(result);
+                        break;
+                    case '-' :
+                        result = num - num2;
+                        System.out.println(result);
+                        break;
+                    case '*' :
+                        result = num * num2;
+                        System.out.println(result);
+                        break;
+                    case '/' :
+                        result = num / num2;
+                        System.out.println(result);
+                        break;
+                }
                 break;
-            case '-' :
-                result = num - num2;
-                System.out.println(result);
-                break;
-            case '*' :
-                result = num * num2;
-                System.out.println(result);
-                break;
-            case '/' :
-                result = num / num2;
-                System.out.println(result);
-                break;
-            default:
-                System.out.println("Incorrect an operation!");
+            }
+
         }
+
     }
 }
